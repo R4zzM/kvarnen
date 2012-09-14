85d975aad4b4e2fa6740ebd681ba9de1cc91ec50
R4zzM <rasmus.m.mattsson@gmail.com>
2012-09-14 17:20:13 +0200

    added missing files

---
+++ app/engine/UidNotFoundException.java
@@ -0,0 +1,13 @@
+package engine;
+
+public class UidNotFoundException extends Exception {
+
+	public UidNotFoundException() {
+		super();
+	}
+	
+	public UidNotFoundException(String message) {
+		super(message);
+	}
+
+}
